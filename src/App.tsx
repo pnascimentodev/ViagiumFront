382f5c071c2bf2c52cc4697ac5dc6207487ce4c0
Myrella Gomes <myrellagomes832@gmail.com>
2025-07-21 14:31:37 -0300

    feat: criação da login/afiliado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import LoginClient from './pages/login/LoginClient'
 import LoginAdmin from './pages/login/LoginAdmin'
+import LoginAffiliate from './pages/login/LoginAffiliate'
 
 function App() {
   return (
     <Routes>
-      <Route path="/client" element={<LoginClient />} />
-      <Route path="/admin" element={<LoginAdmin />} />
+      <Route path="/affiliate" element={<LoginAffiliate />} />
     </Routes>
   )
 }
