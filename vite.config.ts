0275e7fa06d66c2d46d8a2e0ea2af1f6274da860
Priscila Nascimento <pnmelo2808@gmail.com>
2025-08-07 05:07:40 -0300

    chore: update CI/CD workflow and add vite-plugin-static-copy dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      global: 'globalThis',
-    },
-  },
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/img',
+          dest: 'img'
+        }
+      ]
+    })
+  ]
+});
